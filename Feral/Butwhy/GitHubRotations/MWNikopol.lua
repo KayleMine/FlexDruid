b1446ac38b0415a4bb681d8e94d04a5af2226c21
KayleMine <60858771+KayleMine@users.noreply.github.com>
2021-07-15 20:45:24 +0300

    some shi

--- Feral/Butwhy/GitHubRotations/MWNikopol.lua
+++ Feral/Butwhy/GitHubRotations/MWNikopol.lua
@@ -1,15 +1,78 @@
 -- Mistweaver Monk for 8.1 by Nikopol - 12/2018
 -- Talents not in rotation are 1.3, 2.1, 2.2, 2.3, 3.3, 4.2, 4.3, 5.1, 5.2, 5.3, 6.1, 6.3.
 -- Left Shift - Spinning Crane Kick
--- Left Control - Revival
+-- Left Control - jade statue
 -- Left Alt - Essence Font
 local dark_addon = dark_interface
 local SB = m2jue4dgc56acfzz
+local AZ = dark_addon.rotation.spellbooks.azerite
 dark_addon.environment.virtual.exclude_tanks = false
-
+  SB.JadeStatue = 115313
 local soothed_unit
 local lowest_unit
 local tank_unit
+
+local function trink()
+-- attention indian code
+  local wrist = GetInventoryItemID("player", 6)
+  -- local belt = GetInventoryItemID("player", 8)
+  local ring1 = GetInventoryItemID("player", 11)
+  local ring2 = GetInventoryItemID("player", 12)
+  local hands = GetInventoryItemID("player", 7)
+   
+   
+  local Trinket13 = GetInventoryItemID("player", 13)
+  local Trinket14 = GetInventoryItemID("player", 14)
+  local itemID = GetInventoryItemID("player", 13) 
+  local itemID2 = GetInventoryItemID("player", 14) 
+
+
+	if itemID == 169314 then
+
+
+		if GetItemCooldown(Trinket13) == 0 and not player.moving then
+		  macro('/stopcasting')
+		  macro('/use 13')
+		end
+	end
+	
+	
+	if itemID2 == 169314 then
+
+
+		if GetItemCooldown(Trinket14) == 0 and not player.moving then
+		  macro('/stopcasting')
+		  macro('/use 14')
+		end
+	end
+
+    if GetItemCooldown(Trinket13) == 0 and not player.channeling() then
+			--  macro('/stopcasting')
+      macro('/use 13')
+    end
+
+	if GetItemCooldown(Trinket14) == 0 and not player.channeling() then
+			--  macro('/stopcasting')
+			  macro('/use 14')
+	end
+
+
+	if GetItemCooldown(ring1) == 0 and not player.channeling() then
+			  macro('/use 11')
+	end
+	
+	
+		if GetItemCooldown(ring2) == 0  and not player.channeling() then
+			  macro('/use 12')
+	end
+	
+		if GetItemCooldown(hands) == 0  and not player.channeling() then
+			  macro('/use 7')
+	end
+	end
+setfenv(trink, dark_addon.environment.env)
+
+
 
 local function gcd()
   if not player.alive then return end
@@ -64,6 +127,8 @@ local function gcd()
   end
 end
 
+SB.Chiji = 198664
+SB.JadeWind = 196725
 local function combat()
   if not player.alive then return end
   
@@ -75,6 +140,173 @@ local function combat()
   local crackling_jade_lightning = dark_addon.settings.fetch('mw_nikopol_crackling_jade_lightning', false)
   lowest_unit = lowest
   tank_unit = tank
+  if trink() then return end
+  
+  if talent(6, 1) and modifier.lcontrol and castable(SB.JadeStatue) then
+		cast(SB.JadeStatue, 'player')
+  end
+
+  if talent(6, 2) and modifier.lcontrol and castable(SB.JadeWind) then
+		cast(SB.JadeWind, 'player')
+  end
+  
+  if talent(6, 3) and modifier.lcontrol and castable(SB.Chiji) then
+		cast(SB.Chiji, 'player')
+  end
+  
+  
+  if player.alive and target.alive and target.enemy then
+     --Essence start
+   	local delay = 0
+   
+   --burst essences
+
+	--1
+
+    if castable(AZ.GuardianofAzeroth1) and -spell(AZ.GuardianofAzeroth1) == 0 then
+    return cast(AZ.GuardianofAzeroth1, 'target')
+    end 
+	    if castable(AZ.GuardianofAzeroth2) and -spell(AZ.GuardianofAzeroth2) == 0 then
+        return cast(AZ.GuardianofAzeroth2, 'target')
+		end 
+		    if castable(AZ.GuardianofAzeroth3) and -spell(AZ.GuardianofAzeroth3) == 0 then
+			return cast(AZ.GuardianofAzeroth3, 'target')
+			end 
+	
+	--2
+	
+    if castable(AZ.MemoryofLucidDreams1) and -spell(AZ.MemoryofLucidDreams1) == 0 then
+    return cast(AZ.MemoryofLucidDreams1, 'target')
+    end 
+	    if castable(AZ.MemoryofLucidDreams2) and -spell(AZ.MemoryofLucidDreams2) == 0 then
+        return cast(AZ.MemoryofLucidDreams2, 'target')
+		end 
+		    if castable(AZ.MemoryofLucidDreams3) and -spell(AZ.MemoryofLucidDreams3) == 0 then
+			return cast(AZ.MemoryofLucidDreams3, 'target')
+			end 
+
+	--3 uncomment to cast (req. 8.3.0+ patch)
+	
+		if delay < GetTime() then
+	if castable(AZ.MomentofGlory1) then
+    return cast(AZ.MomentofGlory1, 'player')
+    end 
+	    if castable(AZ.MomentofGlory2) and -spell(AZ.MomentofGlory2) == 0 then
+        return cast(AZ.MomentofGlory2, 'target')
+		end 
+		    if castable(AZ.MomentofGlory3) and -spell(AZ.MomentofGlory3) == 0 then
+			return cast(AZ.MomentofGlory3, 'target')
+			end 
+		delay = GetTime() + 1.5
+	end	
+			
+	--4
+	
+    if castable(AZ.WorldveinResonance1) and -spell(AZ.WorldveinResonance1) == 0 then
+    return cast(AZ.WorldveinResonance1, 'target')
+    end 
+	    if castable(AZ.WorldveinResonance2) and -spell(AZ.WorldveinResonance2) == 0 then
+        return cast(AZ.WorldveinResonance2, 'target')
+		end 
+		    if castable(AZ.WorldveinResonance3) and -spell(AZ.WorldveinResonance3) == 0 then
+			return cast(AZ.WorldveinResonance3, 'target')
+			end 
+
+	
+	
+	
+	--damage essences
+	
+	
+	
+	--1 
+	
+	 if castable(AZ.AnimaofDeath1) and -spell(AZ.AnimaofDeath1) == 0 then
+        return cast(AZ.AnimaofDeath1, 'target')
+    end
+			 if castable(AZ.AnimaofDeath2) and -spell(AZ.AnimaofDeath2) == 0 then
+        return cast(AZ.AnimaofDeath2, 'target')
+    end
+				 if castable(AZ.AnimaofDeath3) and -spell(AZ.AnimaofDeath3) == 0 then
+        return cast(AZ.AnimaofDeath3, 'target')
+    end
+	
+	--2 
+	
+	 if castable(AZ.BloodoftheEnemy1) and -spell(AZ.BloodoftheEnemy1) == 0 then
+        return cast(AZ.BloodoftheEnemy1, 'target')
+    end
+			 if castable(AZ.BloodoftheEnemy2) and -spell(AZ.BloodoftheEnemy2) == 0 then
+        return cast(AZ.BloodoftheEnemy2, 'target')
+    end
+				 if castable(AZ.BloodoftheEnemy3) and -spell(AZ.BloodoftheEnemy3) == 0 then
+        return cast(AZ.BloodoftheEnemy3, 'target')
+    end
+	
+		
+	--3  uncomment to cast (req. 8.3.0+ patch)
+	
+	 if castable(AZ.ReapingFlames1) and -spell(AZ.ReapingFlames1) == 0 then
+        return cast(AZ.ReapingFlames1, 'target')
+    end
+			 if castable(AZ.ReapingFlames2) and -spell(AZ.ReapingFlames2) == 0 then
+        return cast(AZ.ReapingFlames2, 'target')
+    end
+				 if castable(AZ.ReapingFlames3) and -spell(AZ.ReapingFlames3) == 0 then
+        return cast(AZ.ReapingFlames3, 'target')
+    end
+	
+	--4
+	if delay < GetTime() then
+	 if castable(AZ.FocusedAzeriteBeam1) and -spell(AZ.FocusedAzeriteBeam1) == 0 then
+        return cast(AZ.FocusedAzeriteBeam1, 'target')
+    end
+			 if castable(AZ.FocusedAzeriteBeam2) and -spell(AZ.FocusedAzeriteBeam2) == 0 then
+        return cast(AZ.FocusedAzeriteBeam2, 'target')
+    end
+				 if castable(AZ.FocusedAzeriteBeam3) and -spell(AZ.FocusedAzeriteBeam3) == 0 then
+        return cast(AZ.FocusedAzeriteBeam3, 'target')
+    end
+	 delay = GetTime() + 1.4
+end	
+	--5
+	
+	 if castable(AZ.PurifyingBlast1) and -spell(AZ.PurifyingBlast1) == 0 then
+        return cast(AZ.PurifyingBlast1, 'target')
+    end
+			 if castable(AZ.PurifyingBlast2) and -spell(AZ.PurifyingBlast2) == 0 then
+        return cast(AZ.PurifyingBlast2, 'target')
+    end
+				 if castable(AZ.PurifyingBlast3) and -spell(AZ.PurifyingBlast3) == 0 then
+        return cast(AZ.PurifyingBlast3, 'target')
+    end
+	
+	--6
+	
+	 if castable(AZ.ConcentratedFlame1) and -spell(AZ.ConcentratedFlame1) == 0 then
+        return cast(AZ.ConcentratedFlame1, 'target')
+    end
+			 if castable(AZ.ConcentratedFlame2) and -spell(AZ.ConcentratedFlame2) == 0 then
+        return cast(AZ.ConcentratedFlame2, 'target')
+    end
+				 if castable(AZ.ConcentratedFlame3) and -spell(AZ.ConcentratedFlame3) == 0 then
+        return cast(AZ.ConcentratedFlame3, 'target')
+    end
+	
+	--7
+			
+	 if castable(AZ.TheUnboundForce1) and -spell(AZ.TheUnboundForce1) == 0 then
+        return cast(AZ.TheUnboundForce1, 'target')
+    end
+			 if castable(AZ.TheUnboundForce2) and -spell(AZ.TheUnboundForce2) == 0 then
+        return cast(AZ.TheUnboundForce2, 'target')
+    end
+				 if castable(AZ.TheUnboundForce3) and -spell(AZ.TheUnboundForce3) == 0 then
+        return cast(AZ.TheUnboundForce3, 'target')
+    end
+	end
+  
+  
   
   if player.channeling(SB.SoothingMist) then
     soothed_unit = group.match(function (unit)
@@ -131,9 +363,9 @@ local function combat()
     return cast(SB.EssenceFont)
   end
 
-  if modifier.lcontrol and castable(SB.Revival) then
-    return cast(SB.Revival)
-  end
+  -- if modifier.lcontrol and castable(SB.Revival) then
+    -- return cast(SB.Revival)
+  -- end
 
   if player.channeling(SB.EssenceFont) then return end
   
@@ -292,6 +524,16 @@ end
 
 local function resting()
   if not player.alive then return end
+   
+  if talent(6, 1) and modifier.lcontrol and castable(SB.JadeStatue) then
+		cast(SB.JadeStatue, 'player')
+  end
+
+  if talent(6, 2) and modifier.lcontrol and castable(SB.JadeWind) then
+		cast(SB.JadeWind, 'player')
+  end
+  
+
   
   local envelope_mist = dark_addon.settings.fetch('mw_nikopol_envelope_mist', 50)
   local vivify = dark_addon.settings.fetch('mw_nikopol_vivify', 70)
@@ -453,6 +695,9 @@ function interface()
       { type = 'header', text = 'Mistweaver Settings' },
       { type = 'rule' },   
       { type = 'text', text = 'Healing Settings' },
+	   { type = 'text', text = 'Control: JadeWind, Statue, Chi-ji' },
+	   { type = 'text', text = 'Alt: Essence Font' },
+	   { type = 'text', text = 'Shift: Spinning Crane Kick' },
       { key = 'envelope_mist', type = 'spinner', text = 'Envelope Mist', desc = 'Cast Envelope Mist on target below % health', min = 30, max = 80, step = 5, default = 55 },
       { key = 'vivify', type = 'spinner', text = 'Vivify', desc = 'Cast Vivify on target below % health', min = 50, max = 90, step = 5, default = 85 },
       { key = 'renewing_mist', type = 'spinner', text = 'Renewing Mist', desc = 'Cast Renewing Mist on target below % health', min = 70, max = 100, step = 5, default = 95 },
@@ -470,12 +715,12 @@ function interface()
     name = 'dispell',
     label = 'Auto Dispell',
     on = {
-      label = 'DSP',
+      label = 'Dispel',
       color = dark_addon.interface.color.green,
       color2 = dark_addon.interface.color.green
     },
     off = {
-      label = 'dsp',
+      label = 'Dispel',
       color = dark_addon.interface.color.grey,
       color2 = dark_addon.interface.color.dark_grey
     }
@@ -511,5 +756,6 @@ dark_addon.rotation.register({
   gcd = gcd,
   combat = combat,
   resting = resting,
-  interface = interface
+  interface = interface,
+    trink = trink
 })
